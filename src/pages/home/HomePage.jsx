acde0dfe83422bf2ffc72014d3fc88973506a2f8
ayanaser4 <nasaraya4@gmail.com>
2025-07-26 02:22:47 +0300

    herosection

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,12 +1,10 @@
-import { useTranslation } from "react-i18next";
+import React from 'react';
+import Hero from '../../components/home/HeroSection/hero';
 export default function HomePage() {
-  const { t } = useTranslation();
 
   return (
     <div>
-      <h1 className="text-3xl font-bold text-center my-8">
-        {t("home.welcome")}
-      </h1>
+      <Hero />
     </div>
   );
 }
