9ea06bc05bb2188612fb88ce41ff56e6a2722a24
ayanaser4 <nasaraya4@gmail.com>
2025-07-28 17:49:12 +0300

    upadate

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -3,8 +3,12 @@ import { useTranslation } from "react-i18next";
 export default function AboutUs() {
       const { t } = useTranslation();
   return (
-    <div>
-      <h1>AboutUs</h1>
+    <div className=" my-12  flex items-center justify-center h-screen bg-gray-100">
+        <h1>{t("About Us")}</h1>
+        <h1>sssssss</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quod, voluptatum quo minima aut nemo dicta modi unde temporibus sit, voluptates pariatur aliquid ea natus dolore possimus, ad tempora blanditiis!</p>
+        <p>ayayayya</p>
     </div>
+    
   )
 }
